2d30eb663cfbc9100207e808641bdd5ec136e5f3
Ryan <rzlim@gmail.com>
2019-07-27 17:14:53 -0400

    fix arguments

--- pyset/__main__.py
+++ pyset/__main__.py
@@ -2,7 +2,8 @@
 import argparse
 
 if __name__ == "__main__":
+    """read in csvs and options"""
     parser = argparse.ArgumentParser(description="performs set operations on csvs")
-    parser.add_argument("csv", nargs="*")
+    parser.add_argument("csvs", nargs="*")
 
     print(parser.parse_args())
